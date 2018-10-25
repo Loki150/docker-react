9c8377f47bd67f32276639f2f8a63e837405e0a4
Sergey Vanskov <sergey.vanskov@lifestreet.com>
2018-10-25 17:24:33 +0300

    first v

---
+++ build/precache-manifest.d13591517df548f5b04af2092bbaa472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f33ea6b8cea9fe6bfa6",
+    "url": "/static/js/main.7f33ea6b.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "7f33ea6b8cea9fe6bfa6",
+    "url": "/static/css/main.79223929.chunk.css"
+  },
+  {
+    "revision": "d67dc6757b35359572ddc936acb0dd4b",
+    "url": "/index.html"
+  }
+];
